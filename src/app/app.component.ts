f794dc258d90cb213aa6ecfbd3ea1a6f3de4922e
unknown <bugrovka12@ukr.net>
2019-02-26 23:01:22 +0200

    working map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'test-maps';
+  map: any;
+  uluru = {lat: -25.344, lng: 131.036};
+  
+      ngOnInit() {
+       this.map = new google.maps.Map(
+        document.getElementById('map'), {zoom: 4, center: this.uluru});  
+      }
 }
